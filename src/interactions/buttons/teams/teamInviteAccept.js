e021c226cfb6d8b38c62707de39012d0732bf2f5
Nachozll <antonio.live2007@gmail.com>
2026-09-10 23:08:39 -0300

    Add teamInviteAccept handler to button interactions

---
+++ src/interactions/buttons/teams/teamInviteAccept.js
@@ -0,0 +1,8 @@
+import {
+    teamInviteAcceptHandler,
+} from '../../../handlers/teamInviteButtons.js';
+
+export default {
+    name: teamInviteAcceptHandler.name,
+    execute: teamInviteAcceptHandler.execute,
+};
