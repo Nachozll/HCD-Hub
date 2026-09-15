05718bb2f34f3555e48fc5f6352cf6eaf86d6506
Nachozll <antonio.live2007@gmail.com>
2026-09-15 00:19:29 -0300

    Create teamApplicationStart.js

---
+++ src/interactions/buttons/teams/teamApplicationStart.js
@@ -0,0 +1,8 @@
+import {
+    teamApplicationStartHandler,
+} from '../../../handlers/teamApplicationStartButton.js';
+
+export default {
+    name: teamApplicationStartHandler.name,
+    execute: teamApplicationStartHandler.execute,
+};
