4c506acb9e8922262de7b45aa4200ec4f1374d15
Nachozll <antonio.live2007@gmail.com>
2026-09-15 23:10:31 -0300

    Add teamApplicationType.js for button handling

---
+++ src/interactions/buttons/teams/teamApplicationType.js
@@ -0,0 +1,8 @@
+import {
+    teamApplicationTypeHandler,
+} from '../../../handlers/teamApplicationTypeButton.js';
+
+export default {
+    name: teamApplicationTypeHandler.name,
+    execute: teamApplicationTypeHandler.execute,
+};
