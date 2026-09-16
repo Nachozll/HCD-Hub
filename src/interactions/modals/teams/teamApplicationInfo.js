d7a0e46d64d7eeba20279ed2af82cb008876fc80
Nachozll <antonio.live2007@gmail.com>
2026-09-15 23:18:22 -0300

    Create teamApplicationInfo.js

---
+++ src/interactions/modals/teams/teamApplicationInfo.js
@@ -0,0 +1,9 @@
+import {
+    teamApplicationInfoModalHandler,
+} from '../../../handlers/teamApplicationInfoModal.js';
+
+export default {
+    name: teamApplicationInfoModalHandler.name,
+    execute:
+        teamApplicationInfoModalHandler.execute,
+};
