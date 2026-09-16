cf74cc8eefcea869633af1dd7fbb577e6827bcbd
Nachozll <antonio.live2007@gmail.com>
2026-09-16 02:27:36 -0300

    Add team application approve button handler

---
+++ src/interactions/buttons/teams/teamApplicationApprove.js
@@ -0,0 +1,11 @@
+import {
+    teamApplicationApproveButtonHandler,
+} from '../../../handlers/teamApplicationApproveButton.js';
+
+export default {
+    name:
+        teamApplicationApproveButtonHandler.name,
+
+    execute:
+        teamApplicationApproveButtonHandler.execute,
+};
