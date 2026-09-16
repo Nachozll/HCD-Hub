dccc01d950130ee2256aa827635eb11efb97ed61
Nachozll <antonio.live2007@gmail.com>
2026-09-16 01:55:11 -0300

    Create teamApplicationSubmit.js

---
+++ src/interactions/buttons/teams/teamApplicationSubmit.js
@@ -0,0 +1,11 @@
+import {
+    teamApplicationSubmitButtonHandler,
+} from '../../../handlers/teamApplicationSubmitButton.js';
+
+export default {
+    name:
+        teamApplicationSubmitButtonHandler.name,
+
+    execute:
+        teamApplicationSubmitButtonHandler.execute,
+};
